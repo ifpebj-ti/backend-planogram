4e3c4bfbe2d6bde7eef07a51f98d9f95f5b84b35
Andressa-Limas <agfl@discente.ifpe.edu.br>
2024-11-06 15:18:54 -0300

    remoção do arquivo eslint.config.js

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,7 +1,10 @@
+const parser = require('@typescript-eslint/parser');
+
 module.exports = [
   {
+    files: ["src/**/*.ts", "apps/**/*.ts", "libs/**/*.ts", "test/**/*.ts"],
     languageOptions: {
-      parser: require('@typescript-eslint/parser'),
+      parser,
       parserOptions: {
         project: 'tsconfig.json',
         tsconfigRootDir: __dirname,
@@ -9,23 +12,47 @@ module.exports = [
       },
     },
     plugins: {
-      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
-      'react': require('eslint-plugin-react'), 
+      '@typescript-eslint': {},
     },
     rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      "@typescript-eslint/no-unused-vars": "off" 
     },
-    settings: {
-      react: {
-        version: '^18.3.1', 
-      },
-    },
-    files: ['src/**/*.{ts,tsx}', 'test/**/*.{ts,tsx}'],
-    ignores: ['dist'],
   },
-  require('eslint-config-prettier'),
+  {
+    ignores: [
+      "build/**",
+      "coverage/**",
+      "docs/!(src|tools)/",
+      "docs/src/!(_data)",
+      "jsdoc/**",
+      "lib/types/**/*.ts",
+      "templates/**",
+      "tests/bench/**",
+      "tests/fixtures/**",
+      "tests/performance/**",
+      "tmp/**",
+      "**/test.js",
+      ".vscode",
+    ],
+  },
+  {
+    files: ["tools/*.js", "docs/tools/*.js"],
+    rules: {
+      "no-console": "off",
+      "n/no-process-exit": "off",
+    },
+  },
+  {
+    files: ["lib/rules/*.js", "tools/internal-rules/*.js"],
+    ignores: ["**/index.js"],
+    rules: {
+      "eslint-plugin/prefer-placeholders": "error",
+      "eslint-plugin/prefer-replace-text": "error",
+      "eslint-plugin/report-message-format": ["error", "[^a-z].*\\.$"],
+      "eslint-plugin/require-meta-docs-description": ["error", { pattern: "^(Enforce|Require|Disallow) .+[^. ]$" }],
+    },
+  },
 ];
